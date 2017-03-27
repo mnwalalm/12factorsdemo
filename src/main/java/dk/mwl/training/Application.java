f137dee4294e1fb949eb75dc659559e406bca11d
mnwalalm <martin-nwalal-mi-nyom@capgemini.com>
2017-03-27 13:30:46 +0100

    Adding new chnages

--- src/main/java/dk/mwl/training/Application.java
+++ src/main/java/dk/mwl/training/Application.java
@@ -1,11 +1,19 @@
 package dk.mwl.training;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
+	@Bean
+	public CommandLineRunner  cliRunner(@Value("${user.name}") String userName){
+		return args -> System.out.println("Hello "+ userName);
+		
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
